b9febc31bb690164fc6baa3b26dfd120bc603deb
Anoop Ambooken <anoop.ambooken@infor.com>
2022-06-17 01:15:23 +0530

    refactoring
    move a the compute of live neighbours of a grid to a new function for readability

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-#Instructions to run the project
+
+# Instructions to run the project
 Make sure you have installed Java11
 
 Run following commands to execute the main java class
@@ -8,10 +9,12 @@ javac com/chargepoint/game/ConwayGameOfLife.java
 java com.chargepoint.game.ConwayGameOfLife
 ```
 
-#Assumptions Made
+# Assumptions Made
 1) The dead cells in grid is represented by digit '0'
 2) The alive cells in grid is represented by digit '1'
 3) Modify the following variable in the java class to change the number of ticks (have not made it interactive by accepting an integer from user)
 ```java
 int numberOfTicks = 3;
+
 ```
+4) Every cell is grid is filled with either '0' or '1' and no other value
--- src/com/chargepoint/game/ConwayGameOfLife.java
+++ src/com/chargepoint/game/ConwayGameOfLife.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 public class ConwayGameOfLife {
     public static void main(String args[]) {
         int numberOfTicks = 3; //modify this field to change the number of ticks
-        int noOfRowsInGrid = 4;
-        int numberOfColumnsInGrid = 5;
+
 
         //Initialise grid, assuming dead cells in grid as 0 and alive cells as 1
         // We define a fixed grid size of rows and columns instead of an infinite one
@@ -15,6 +14,9 @@ public class ConwayGameOfLife {
                 {0, 1, 1, 1, 0},
                 {0, 0, 0, 0, 0}
         };
+
+        int noOfRowsInGrid = seedGrid.length;
+        int numberOfColumnsInGrid = seedGrid[0].length;
 
         //printing seedGrid
         System.out.println("Initial State of Universe");
@@ -46,36 +48,21 @@ public class ConwayGameOfLife {
         for (int row = 0; row < noOfRowsInGrid; row++) {
             for (int column = 0; column < numberOfColumnsInGrid; column++) { //iterate one by one the cells in grid
 
-                int numberOfLiveNeighbourCells = 0;
+                int numberOfLiveNeighbourCells = computeNumberOfLiveNeighbourCells(seedGrid, row, column,
+                        noOfRowsInGrid, numberOfColumnsInGrid);
 
-                //iterate to find 8 neighbours of current cell
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        if ((row + i) >= 0 && (row + i) < noOfRowsInGrid  // check if there are no row overflows from grid
-                                && (column + j) >= 0 && (column + j) < numberOfColumnsInGrid //check no column overflows
-                            //  && (i != 0 && j != 0)
-                        ) //check if not current grid
-                            numberOfLiveNeighbourCells = numberOfLiveNeighbourCells + seedGrid[row + i][column + j];
-                    }
-                }
-
-                numberOfLiveNeighbourCells = numberOfLiveNeighbourCells - seedGrid[row][column];
-                
                 //case for under population
                 if (seedGrid[row][column] == 1 && numberOfLiveNeighbourCells < 2) {
                     nextGenGrid[row][column] = 0;
                 }
-
                 //lives to next generation
                 else if (seedGrid[row][column] == 1 && (numberOfLiveNeighbourCells == 2 || numberOfLiveNeighbourCells == 3)) {
                     nextGenGrid[row][column] = 1;
                 }
-
                 //case of overcrowding
                 else if (seedGrid[row][column] == 1 && (numberOfLiveNeighbourCells > 3)) {
                     nextGenGrid[row][column] = 0;
                 }
-
                 //case of reproduction
                 else if (seedGrid[row][column] == 0 && (numberOfLiveNeighbourCells == 3)) {
                     nextGenGrid[row][column] = 1;
@@ -87,5 +74,25 @@ public class ConwayGameOfLife {
         }
 
         return nextGenGrid;
+    }
+
+    static int computeNumberOfLiveNeighbourCells(int[][] seedGrid, int row, int column,
+                                                 int noOfRowsInGrid, int numberOfColumnsInGrid) {
+        int numberOfLiveNeighbourCells = 0;
+
+        //iterate to find 8 neighbours of current cell
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if ((row + i) >= 0 && (row + i) < noOfRowsInGrid  // check if there are no row overflows from grid
+                        && (column + j) >= 0 && (column + j) < numberOfColumnsInGrid) //check no column overflows
+                {
+                    numberOfLiveNeighbourCells = numberOfLiveNeighbourCells + seedGrid[row + i][column + j];
+                }
+            }
+        }
+        //remove the seedGrid[row][column] from the computation
+        numberOfLiveNeighbourCells = numberOfLiveNeighbourCells - seedGrid[row][column];
+
+        return numberOfLiveNeighbourCells;
     }
 }
